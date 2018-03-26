222e53b569b2fafff5ea29eeaf087aacb9a513da
MissHu <34367530+MissYingYingHu@users.noreply.github.com>
2018-03-26 23:40:20 +0800

    哈希表之开散列

--- test.c
+++ test.c
@@ -1,67 +1,157 @@
-#include"MiniStack.h"
+#include"HashTable2.h"
 #include<stdio.h>
 #include<windows.h>
-
-#define TEST_HEADER printf("\n===================%s==================\n",__FUNCTION__)
-void Test()
+void PrintHash(HashTable* ht)
 {
-	mini_stack stack;
-	MiniStackType top;
-	int ret = 0;
-	int ret1 = 0;
-	int ret2 = 0;
-	TEST_HEADER;
-	InitMiniStack(&stack);
-	PushMiniStack(&stack,'e');
-	PushMiniStack(&stack,'s');
-	PushMiniStack(&stack,'a');
-	PushMiniStack(&stack,'c');
-	PushMiniStack(&stack,'b');
-	ret = TopMiniStack(&stack,&top);
-	//printf("ret expect is 1,actual is %d\n",ret);
-	printf("top expect is a,actual is %c\n",top);
-	PopMiniStack(&stack);
-	PopMiniStack(&stack);
-	PopMiniStack(&stack);
-	ret1 = TopMiniStack(&stack,&top);
-	//printf("ret expect is 1,actual is %d\n",ret1);
-	printf("top expect is e,actual is %c\n",top);
-	PopMiniStack(&stack);
-
-	ret2 = TopMiniStack(&stack,&top);
-	//printf("ret expect is 1,actual is %d\n",ret2);
-	printf("top expect is e,actual is %c\n",top);
-	PopMiniStack(&stack);
+	HashElem* cur = NULL;
+	size_t i = 0;
+	if(ht == NULL)
+	{
+		return;
+	}
+	for(;i < HashMaxSize;i++)
+	{
+		if(ht->data [i] != NULL)
+		{
+			cur = ht->data [i];
+			while(cur)
+			{
+				printf("[%d] key is %d,value is %d.",i,cur->key ,cur->value);
+				cur = cur->next ;
+			}
+			printf("\n");
+		}
+	}
 }
-void Test2()
+
+void TESTInit()
 {
-	MiniStack stack;
-	MiniType top;
-	int ret = 0;
+	size_t i = 0;
 	TEST_HEADER;
-	InitMini(&stack);
-	PushMini(&stack,'e');
-	PushMini(&stack,'s');
-	PushMini(&stack,'a');
-	PushMini(&stack,'c');
-	PushMini(&stack,'b');
-	ret = TopMini(&stack,&top);
-	//printf("ret expect is 1,actual is %d\n",ret);
-	printf("top expect is a,actual is %c\n",top);
-	PopMini(&stack);
-	PopMini(&stack);
-	PopMini(&stack);
-	ret = TopMini(&stack,&top);
-	//printf("ret expect is 1,actual is %d\n",ret);
-	printf("top expect is e,actual is %c\n",top);
-	PopMini(&stack);
-	TopMini(&stack,&top);
-	printf("top expect is e,actual is %c\n",top);
+	HashTableInit(&ht,&func);
+	for(;i < HashMaxSize;i++)
+	{
+		if(ht.data [i] != NULL)
+		{
+			printf("%d is errror!\n",i);
+		}
+	}
+	printf("size expect is 0,actual is %d.\n",ht.size );
+	printf("hash_func expect is %p,actual is %p.\n",func,ht.hash_func );
+}
+void TESTInsert()
+{
+	TEST_HEADER;
+	HashTableInit(&ht,&func);
+	HashInsert(&ht,1,100);
+	HashInsert(&ht,2,200);
+	HashInsert(&ht,1001,300);
+	HashInsert(&ht,1002,400);
+	HashInsert(&ht,1001,500);
+	PrintHash(&ht);
+}
+void TESTFind()
+{
+	int ret = 0;
+	int value = 0;
+	TEST_HEADER;
+	HashTableInit(&ht,&func);
+	HashInsert(&ht,1,100);
+	HashInsert(&ht,2,200);
+	HashInsert(&ht,1001,300);
+	HashInsert(&ht,1002,400);
+	HashInsert(&ht,1001,500);
+	PrintHash(&ht);
+	ret = HashFind(&ht,1001,&value);
+	printf("ret expect is 1,actual is %d.\n",ret);
+	printf("value expect is 300,actual is %d\n",value);
+}
+void TESTRemove()
+{
+	TEST_HEADER;
+	HashTableInit(&ht,&func);
+	HashInsert(&ht,1,100);
+	HashInsert(&ht,2,200);
+	HashInsert(&ht,1001,300);
+	HashInsert(&ht,1002,400);
+	HashInsert(&ht,1003,500);
+	PrintHash(&ht);
+	printf("\n");
+	HashRemove(&ht,1001);
+	PrintHash(&ht);
+	printf("\n");
+	HashRemove(&ht,1002);
+	PrintHash(&ht);
+	printf("\n");
+	HashRemove(&ht,1003);
+	PrintHash(&ht);
+
+}
+void TESTSize()
+{
+	size_t ret = 0;
+	TEST_HEADER;
+	HashTableInit(&ht,&func);
+	HashInsert(&ht,1,100);
+	HashInsert(&ht,2,200);
+	HashInsert(&ht,1001,300);
+	HashInsert(&ht,1002,400);
+	HashInsert(&ht,1003,500);
+	PrintHash(&ht);
+	ret = HashSize(&ht);
+	printf("size is %d\n",ret);
+}
+
+void TESTEmpty()
+{
+	size_t ret = 0;
+	TEST_HEADER;
+	HashTableInit(&ht,&func);
+	HashInsert(&ht,1,100);
+	HashInsert(&ht,2,200);
+	HashInsert(&ht,1001,300);
+	HashInsert(&ht,1002,400);
+	HashInsert(&ht,1003,500);
+	PrintHash(&ht);
+	ret = HashEmpty(&ht);
+	printf("expect is 0,actual is %d\n",ret);
+}
+void TESTDestroy()
+{
+	size_t i = 0;
+	TEST_HEADER;
+	HashTableInit(&ht,&func);
+	HashInsert(&ht,1,100);
+	HashInsert(&ht,2,200);
+	HashInsert(&ht,1001,300);
+	HashInsert(&ht,1002,400);
+	HashInsert(&ht,1003,500);
+	PrintHash(&ht);
+	HashDestroy(&ht);
+	for(;i < HashMaxSize;i++)
+	{
+		if(ht.data [i] != NULL)
+		{
+			while(ht.data [i]->next )
+			{
+				printf("%d is errror!\n",i);
+				ht.data [i] = ht.data[i]->next ;
+			}
+			//printf("%d is errror!\n",i);
+		}
+	}
+	printf("size expect is 0,actual is %d.\n",ht.size );
+	printf("hash_func expect is NULL,actual is %p.\n",ht.hash_func );
 }
 int main()
 {
-	Test();
-	Test2();
+	TESTInit();
+	TESTInsert();
+	TESTFind();
+	TESTRemove();
+	TESTSize();
+	TESTEmpty();
+	TESTDestroy();
 	system("pause");
 	return 0;
 }
