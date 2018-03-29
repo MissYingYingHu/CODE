9ee96a09b00153aba6c7d1caf0f1bc6f29d7d1a6
MissHu <34367530+MissYingYingHu@users.noreply.github.com>
2018-03-29 11:18:54 +0800

    堆栈面试题之两个栈实现一个队列

--- test.c
+++ test.c
@@ -1,33 +1,42 @@
-#include"Stack.h"
+#include"Queue.h"
 #include<stdio.h>
 #include<windows.h>
-//#define TEST_HEADER printf("\n====================%s====================\n",__FUNCTION__)
 
+#define TEST_HEADER printf("\n================%s================\n",__FUNCTION__)
 void Test()
 {
-	StackType top;
-	Stack s;
+	Queue q;
+	QueueType top;
 	TEST_HEADER;
-	InitStack(&s);
-	PushStack(&s,'a');
-	PushStack(&s,'b');
-	PushStack(&s,'c');
-	PushStack(&s,'d');
-	TopStack(&s,&top);
-	printf("top:expect is d,actual is %c\n",top);
+	InitQueue(&q);
+	PushQueue(&q,'a');
+	PushQueue(&q,'b');
+	PushQueue(&q,'c');
+	PushQueue(&q,'d');
+	TopQueue(&q,&top);
+	printf("top:expext a,actual is %c\n",top);
 
-	PopStack(&s);
-	TopStack(&s,&top);
-	printf("top:expect is c,actual is %c\n",top);
+	PopQueue(&q);
+	TopQueue(&q,&top);
+	printf("top:expext b,actual is %c\n",top);
 
-	PopStack(&s);
-	TopStack(&s,&top);
-	printf("top:expect is b,actual is %c\n",top);
+	PopQueue(&q);
+	TopQueue(&q,&top);
+	printf("top:expext c,actual is %c\n",top);
 
-	PopStack(&s);
-	TopStack(&s,&top);
-	printf("top:expect is a,actual is %c\n",top);
+	PopQueue(&q);
+	TopQueue(&q,&top);
+	printf("top:expext d,actual is %c\n",top);
+
+	PopQueue(&q);
+	TopQueue(&q,&top);
+	printf("top:expext d,actual is %c\n",top);
+
+	PopQueue(&q);
+	TopQueue(&q,&top);
+	printf("top:expext d,actual is %c\n",top);
 }
+///////////////////////////////////////////////////
 int main()
 {
 	Test();
